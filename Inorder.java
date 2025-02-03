504d9041e73b435dd42446d334e6676c7e408acd
Asjad Samdani <samdaniasjad143f@gmail.com>
2025-02-03 23:27:40 +0530

    create Inorder traversal

---
+++ Inorder.java
@@ -0,0 +1,43 @@
+
+class Node {
+  int data;
+  Node left;
+  Node right;
+
+  public Node(int data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+public class Inorder {
+  static int idx = -1;
+
+  public static Node buildTree(int[] node) {
+    idx++;
+    if (node[idx] == -1)
+      return null;
+    Node newNode = new Node(node[idx]);
+    newNode.left = buildTree(node);
+    newNode.right = buildTree(node);
+    return newNode;
+  }
+
+  public static void inOrder(Node node) {
+    if (node == null)
+      return;
+    inOrder(node.left);
+    System.out.print(node.data + " ");
+    inOrder(node.right);
+
+  }
+
+  public static void main(String[] args) {
+    int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+    Node root = buildTree(nodes);
+    inOrder(root);
+
+  }
+
+}
