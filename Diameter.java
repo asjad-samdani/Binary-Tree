45bc7d77a687af907c183cdf3aff13a3ea785c0a
Asjad Samdani <samdaniasjad143f@gmail.com>
2025-02-03 23:51:01 +0530

    a method to find diameter between two long exists nodes

---
+++ Diameter.java
@@ -0,0 +1,49 @@
+
+class Node {
+  int data;
+  Node left;
+  Node right;
+
+  public Node(int data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+public class Diameter {
+  static int idx = -1;
+
+  public static Node buildTree(int[] node) {
+    idx++;
+    if (node[idx] == -1)
+      return null;
+    Node newNode = new Node(node[idx]);
+    newNode.left = buildTree(node);
+    newNode.right = buildTree(node);
+    return newNode;
+  }
+
+  public static int totalHeight(Node node) {
+    if (node == null)
+      return 0;
+    return Math.max(totalHeight(node.left), totalHeight(node.right)) + 1;
+
+  }
+
+  public static int diameter(Node node) {
+    if (node == null)
+      return 0;
+    int dia1 = diameter(node.left);
+    int dia2 = diameter(node.right);
+    int dia3 = totalHeight(node.left) + totalHeight(node.right) + 1;
+    return Math.max(dia1, Math.max(dia2, dia3));
+  }
+
+  public static void main(String[] args) {
+    int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+    Node root = buildTree(nodes);
+    System.out.println(diameter(root));
+  }
+
+}
