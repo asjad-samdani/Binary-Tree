b9a4569c197eac5776ac4e53bd3b6300553fe16c
Asjad Samdani <samdaniasjad143f@gmail.com>
2025-02-03 23:26:19 +0530

    create postorder Traversal

---
+++ PostOrder.java
@@ -0,0 +1,43 @@
+
+class Node {
+  int data;
+  Node left;
+  Node right;
+
+  public Node(int data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+public class PostOrder {
+  static int idx = -1;
+
+  public static Node buildTree(int[] node) {
+    idx++;
+    if (node[idx] == -1)
+      return null;
+    Node newNode = new Node(node[idx]);
+    newNode.left = buildTree(node);
+    newNode.right = buildTree(node);
+    return newNode;
+  }
+
+  public static void postOrder(Node node) {
+    if (node == null)
+      return;
+    postOrder(node.left);
+    postOrder(node.right);
+    System.out.print(node.data + " ");
+
+  }
+
+  public static void main(String[] args) {
+    int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+    Node root = buildTree(nodes);
+    postOrder(root);
+
+  }
+
+}
